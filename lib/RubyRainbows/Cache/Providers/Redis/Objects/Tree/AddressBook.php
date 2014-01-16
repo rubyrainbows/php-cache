f32dfba133f82f1e9c237baec8739dbc30bfedf1
Thomas Muntaner <thomas.muntaner@gmail.com>
2014-01-16 10:49:27 +0100

    added comments

--- lib/RubyRainbows/Cache/Providers/Redis/Objects/Tree/AddressBook.php
+++ lib/RubyRainbows/Cache/Providers/Redis/Objects/Tree/AddressBook.php
@@ -5,11 +5,26 @@ use RubyRainbows\Cache\Providers\Redis\Client as Client;
 
 class AddressBook
 {
+    /**
+     * Adds an address to the address book
+     *
+     * @param $key
+     * @param $id
+     * @param array $address
+     */
     public static function add($key, $id, array $address=[])
     {
         Client::hset($key,$id, json_encode($address));
     }
 
+    /**
+     * Gets an address from the address book
+     *
+     * @param $key
+     * @param $id
+     *
+     * @return mixed
+     */
     public static function get($key, $id)
     {
         return json_decode(Client::hget($key, $id), false);
